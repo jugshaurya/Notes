d97b7e10f341085b0585b1832a837be3f3bb591f
jugshaurya <shauryasinghal84@gmail.com>
2022-03-10 14:22:02 +0530

    :lion: updated sidebar titles
    added introduction

--- docs/intro.md
+++
@@ -1,8 +0,0 @@
----
-sidebar_label: 'notes hoempage'
-sidebar_position: 0
----
-
-# Notes
-
-- See Sidebar 👈 ⬅️
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: "https://github.com/jugshaurya/Notes/tree/main/docs",
+          editUrl: "https://github.com/jugshaurya/Notes/tree/main",
         },
         blog: {
           showReadingTime: true,
