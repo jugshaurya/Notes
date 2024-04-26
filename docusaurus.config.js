ba835f52a630a83be782323a77651237d67ac4d1
jugshaurya <shauryasinghal84@gmail.com>
2024-04-26 16:34:17 +0530

    chore: updated notes, theme, font

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,112 +6,114 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Shaurya Singhal 🇮🇳",
-  tagline: "Notes that helps you in Interviews and in Competitive Programming.",
-  url: "https://your-docusaurus-test-site.com",
-  baseUrl: "/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
-  organizationName: "shaurya", // Usually your GitHub org/user name.
-  projectName: "Showcase Notes", // Usually your repo name.
+    title: "Shaurya Singhal 🇮🇳",
+    tagline:
+        "Notes that helps you in Interviews and in Competitive Programming.",
+    url: "https://notes.jugshaurya.vercel.app/",
+    baseUrl: "/",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/favicon.ico",
+    organizationName: "shaurya", // Usually your GitHub org/user name.
+    projectName: "Showcase Notes", // Usually your repo name.
 
-  presets: [
-    [
-      "classic",
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
-          editUrl: "https://github.com/jugshaurya/Notes/tree/main",
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl: "https://github.com/jugshaurya/Notes/tree/main/docs",
-        },
-        theme: {
-          customCss: require.resolve("./src/css/custom.css"),
-        },
-      }),
+    presets: [
+        [
+            "classic",
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve("./sidebars.js"),
+                    // Please change this to your repo.
+                    editUrl: "https://github.com/jugshaurya/Notes/tree/main",
+                },
+                blog: {
+                    showReadingTime: true,
+                    // Please change this to your repo.
+                    editUrl:
+                        "https://github.com/jugshaurya/Notes/tree/main/docs",
+                },
+                theme: {
+                    customCss: require.resolve("./src/css/custom.css"),
+                },
+            }),
+        ],
     ],
-  ],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: "Shaurya Showcase",
-        logo: {
-          alt: "My Site Logo",
-          src: "img/shaurya.png",
-        },
-        items: [
-          {
-            type: "doc",
-            docId: "intro",
-            position: "left",
-            label: "See Notes",
-          },
-          { to: "/blog", label: "Blog", position: "left" },
-          {
-            href: "https://github.com/jugshaurya/notes",
-            label: "GitHub",
-            position: "right",
-          },
-        ],
-      },
-      footer: {
-        style: "dark",
-        links: [
-          {
-            title: "Docs",
-            items: [
-              {
-                label: "Notes",
-                to: "/docs/intro",
-              },
-            ],
-          },
-          {
-            title: "Community",
-            items: [
-              {
-                label: "Github",
-                href: "https://github.com/jugshaurya",
-              },
-              {
-                label: "Twitter",
-                href: "https://twitter.com/jugshaurya",
-              },
-              {
-                label: "LinkedIn",
-                href: "https://www.linkedin.com/in/jugshaurya/",
-              },
-            ],
-          },
-          {
-            title: "More",
-            items: [
-              {
-                label: "View my Portfolio",
-                to: "https://www.jugshaurya.in/",
-              },
-              {
-                label: "Mail me: shauryasinghal84@gmail.com",
-                href: "mailto:shauryasinghal84@gmail.com",
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Shaurya Showcase. 💟 Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+    themeConfig:
+        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            navbar: {
+                title: "Shaurya Showcase",
+                logo: {
+                    alt: "My Site Logo",
+                    src: "img/shaurya.png",
+                },
+                items: [
+                    {
+                        type: "doc",
+                        docId: "intro",
+                        position: "left",
+                        label: "Go to Notes",
+                    },
+                    { to: "/blog", label: "Go to Blogs", position: "left" },
+                    {
+                        href: "https://github.com/jugshaurya/notes",
+                        label: "GitHub",
+                        position: "right",
+                    },
+                ],
+            },
+            footer: {
+                style: "dark",
+                links: [
+                    {
+                        title: "Docs",
+                        items: [
+                            {
+                                label: "Notes",
+                                to: "/docs/intro",
+                            },
+                        ],
+                    },
+                    {
+                        title: "Community",
+                        items: [
+                            {
+                                label: "Github",
+                                href: "https://github.com/jugshaurya",
+                            },
+                            {
+                                label: "Twitter",
+                                href: "https://twitter.com/jugshaurya",
+                            },
+                            {
+                                label: "LinkedIn",
+                                href: "https://www.linkedin.com/in/jugshaurya/",
+                            },
+                        ],
+                    },
+                    {
+                        title: "More",
+                        items: [
+                            {
+                                label: "View my Portfolio",
+                                to: "https://jugshaurya.vercel.app/",
+                            },
+                            {
+                                label: "Mail me: shauryasinghal84@gmail.com",
+                                href: "mailto:shauryasinghal84@gmail.com",
+                            },
+                        ],
+                    },
+                ],
+                copyright: `Copyright © ${new Date().getFullYear()} Shaurya Showcase. 💟 Built with Docusaurus.`,
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+            },
+        }),
 };
 
 module.exports = config;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,43 +5,49 @@ import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styles from "./index.module.css";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
-
+import useBaseUrl from "@docusaurus/useBaseUrl";
 import { Analytics } from "@vercel/analytics/react";
 
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
-            Learn from Notes 📚
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+    const { siteConfig } = useDocusaurusContext();
+    return (
+        <header className={clsx("hero hero--primary", styles.heroBanner)}>
+            <div className="container">
+                <h1 className="hero__title">{siteConfig.title}</h1>
+                <p className="hero__subtitle">{siteConfig.tagline}</p>
+                <div className={styles.buttons}>
+                    <Link
+                        className="button button--secondary button--lg"
+                        to="/docs/intro"
+                    >
+                        Learn from Notes 📚
+                    </Link>
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <>
-      <Layout
-        title={`Hello from ${siteConfig.title}`}
-        description="Description will go into a meta tag in <head />"
-      >
-        <HomepageHeader />
-        <main>
-          <HomepageFeatures />
-        </main>
-      </Layout>
-      <Analytics />
-    </>
-  );
+    const { siteConfig } = useDocusaurusContext();
+    const url = useBaseUrl("/docs/intro");
+
+    React.useEffect(() => {
+        window.location.href = url;
+    }, []);
+
+    return (
+        <>
+            <Layout
+                title={`Hello from ${siteConfig.title}`}
+                description="Description will go into a meta tag in <head />"
+            >
+                <HomepageHeader />
+                <main>
+                    <HomepageFeatures />
+                </main>
+            </Layout>
+            <Analytics />
+        </>
+    );
 }
